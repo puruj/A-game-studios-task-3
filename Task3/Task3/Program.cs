6edcf654579d7a958f5f964e5851a09c961743ed
puruj <puru123456789@hotmail.com>
2023-02-27 00:29:09 -0500

    solution works

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿Solution s = new Solution();
+
+int[] A = {0, 1, 0, 1, 1}; //Should return 5
+
+var example1 = s.solution(A);
+
+Console.WriteLine($"The result of example1 is {example1}.");
---
+++ Task3/Task3/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Solution
+{
+    /*
+    * Time complexity:  O(N), since we traverse the input array once and where N is the length of the input array.
+    * Space complexity: O(1), since we modify the input array in place. 
+    */
+    public int solution(int[] A)
+    {
+        var numberOfCarsGoingEast = 0;
+        var numberOfPassingCars = 0;
+
+        foreach (var car in A)
+        {
+            // If car going east, increment the number of cars going east
+            if (car == 0)
+            {
+                numberOfCarsGoingEast++;
+            }
+            // If car is going west, add number of cars going east to the number of passing cars
+            // If number of passing cars exceeds 1,000,000,000, return -1
+            else
+            {
+                numberOfPassingCars += numberOfCarsGoingEast;
+                if (numberOfPassingCars > 1000000000)
+                {
+                    return -1;
+                }
+            }
+        }
+        return numberOfPassingCars;
+    }
+}
+
